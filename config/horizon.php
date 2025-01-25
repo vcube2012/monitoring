c03789cc6efb952b8fb4ad599574412a2210a1e7
tsar <vcube.su@gmail.com>
2025-01-25 21:29:46 +0200

    1

--- config/horizon.php
+++ config/horizon.php
@@ -189,7 +189,7 @@ return [
             'maxTime' => 0,
             'maxJobs' => 0,
             'memory' => 128,
-            'tries' => 1,
+            'tries' => 3,
             'timeout' => 60,
             'nice' => 0,
         ],
